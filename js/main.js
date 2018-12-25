1c5a8c2bc462028121f72099ad670d5152aa117f
Favour Kelvin <favourkelvin17@gmail.com>
2018-12-25 22:42:37 +0100

    changed the font styles and dummy test in about -us and index.html

--- js/main.js
+++ js/main.js
@@ -105,3 +105,14 @@ jQuery(document).ready(function ($) {
     //End
 
 });
+
+$(document).ready(function () {
+    $("#waterwheel-carousel").waterwheelCarousel({
+      horizon: 110,
+      horizonOffset: -50,
+      horizonOffsetMultiplier: .7,
+      separation: 150,
+      edgeFadeEnabled: true
+    });
+  });
+
